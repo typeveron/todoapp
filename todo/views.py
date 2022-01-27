18e1c1783177a6950b8770ee09aaacf2615bab19
Bechir Mihoub <perfectnumber@hotmail.ca>
2022-01-26 23:02:21 -0500

    fifth commit

--- todo/views.py
+++ todo/views.py
@@ -73,7 +73,7 @@ def currenttodos(request):
 
 @login_required
 def completedtodos(request):
-    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False.order_by('-datecompleted'))
+    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
     return render(request, 'todo/completedtodos.html', {'todos': todos})
 
 @login_required
